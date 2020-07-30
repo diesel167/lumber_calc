0a6a188f8981ede1fe9fe2c08f6fcb99de5e4692
Yury Misiyevich <misiyevich@gmail.com>
2020-07-30 21:13:49 +0300

    feat: build

---
+++ precache-manifest.5ccfd1ad1f60238b790d9e08fc4fe23c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e419befee0cb2964a0d0b5253f5056a",
+    "url": "/lumber_calc/index.html"
+  },
+  {
+    "revision": "d8fcc22dba6496b1d1c1",
+    "url": "/lumber_calc/static/css/main.c3149139.chunk.css"
+  },
+  {
+    "revision": "2686e29e5e7660290011",
+    "url": "/lumber_calc/static/js/2.41151a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lumber_calc/static/js/2.41151a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8fcc22dba6496b1d1c1",
+    "url": "/lumber_calc/static/js/main.c29f573f.chunk.js"
+  },
+  {
+    "revision": "8fefb6544d4b02ca62fb",
+    "url": "/lumber_calc/static/js/runtime-main.7ee961c5.js"
+  },
+  {
+    "revision": "250eb04e25e1326faa9918c5f607ecd8",
+    "url": "/lumber_calc/static/media/Worksans.250eb04e.woff2"
+  },
+  {
+    "revision": "36d06126fc396b7b2e9203f59065fb7d",
+    "url": "/lumber_calc/static/media/Worksans.36d06126.ttf"
+  },
+  {
+    "revision": "cd55f56f36c30e2c4f8bfcfdb55254e2",
+    "url": "/lumber_calc/static/media/Worksans.cd55f56f.woff"
+  },
+  {
+    "revision": "094f43586ab136f29ca15894fb7dfe7d",
+    "url": "/lumber_calc/static/media/derevo.094f4358.jpg"
+  },
+  {
+    "revision": "b45e9a51f6b624a428963d324aeaaa1c",
+    "url": "/lumber_calc/static/media/lumber1.b45e9a51.jpg"
+  },
+  {
+    "revision": "fc06f1380f61c3b693942dbf0d4caa06",
+    "url": "/lumber_calc/static/media/lumber2.fc06f138.jpg"
+  },
+  {
+    "revision": "68da841a307830f10e4be6bd76d1bc0c",
+    "url": "/lumber_calc/static/media/lumber3.68da841a.jpg"
+  }
+]);
